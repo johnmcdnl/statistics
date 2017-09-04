ea86b8dfb4d380612cd3f3b6fe6ad34525f1aacf
John McDonnell <john.mcdonnell439@gmail.com>
2017-09-04 20:41:41 +0100

    extend tests

--- median_test.go
+++ median_test.go
@@ -14,12 +14,15 @@ func TestMedian(t *testing.T) {
 		args args
 		want float64
 	}{
-		{"t1", args{values: []float64{96, 100, 106, 114}}, 103},
+		{"even", args{values: []float64{96, 100, 106, 114}}, 103},
+		{"odd", args{values: []float64{3, 5, 12}}, 5},
+		{"even unsorted", args{values: []float64{3, 13, 7, 5, 21, 23, 39, 23, 40, 23, 14, 12, 56, 23, 29}}, 23},
+		{"odd unsorted", args{values: []float64{3, 13, 7, 5, 21, 23, 23, 40, 23, 14, 12, 56, 23, 29}}, 22},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := Median(tt.args.values)
-			assert.InEpsilon(t, tt.want, got, allowedEpsilon)
+			assert.InEpsilon(t, tt.want, got, allowedEpsilon, fmtError(tt.args.values, tt.want, got))
 		})
 	}
 }
